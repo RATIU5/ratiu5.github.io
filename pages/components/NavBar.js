b76dd6d96112f261787c5e1f8fcd65fcdfb3e929
johnratius <jasonmemm@gmail.com>
2020-09-04 13:45:49 -0700

    Updated styles

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -9,32 +9,22 @@ export default function NavBar()
     return(
         <div className={styles.navBarContainer}>
 
-            <label htmlFor="menuCheck" className={styles.menuCheckIcon}><MdMenu /></label>
             <input type="checkbox" id="menuCheck" className={styles.menuCheckbox}/>
+            <label htmlFor="menuCheck" className={styles.menuCheckIcon}><MdMenu /></label>
 
             <div className={styles.navMenuContainer}>
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>
-                                <div className={styles.navLinksContainer}>
-                                    <div className={styles.navLink}>
-                                        <a href="#linkProjects" >projects</a>
-                                    </div>
-                                    <div className={styles.navLink}>
-                                        <a href="#linkAbout">about</a>
-                                    </div>
-                                    <div className={styles.navLink}>
-                                        <a href="#linkContact">contact</a>
-                                    </div>
-                                </div>
-                            </th>
-                            <th>
-                                <SocialLinks></SocialLinks>
-                            </th>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className={styles.navLinksContainer}>
+                    <div className={styles.navLink}>
+                        <a href="#linkProjects" >projects</a>
+                    </div>
+                    <div className={styles.navLink}>
+                        <a href="#linkAbout">about</a>
+                    </div>
+                    <div className={styles.navLink}>
+                        <a href="#linkContact">contact</a>
+                    </div>
+                </div>
+                <SocialLinks></SocialLinks>
             </div>
         </div>
     )
